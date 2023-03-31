289c5e1aa4daf18c7fcb41e2ac9463ac052a8560
nseah21 <nseah21@gmail.com>
2023-04-01 00:43:53 +0800

    Add basic UI

--- src/Logic.java
+++ src/Logic.java
@@ -1,9 +1,24 @@
+import java.lang.Thread;
+
 class Logic {
-    private Event[] events = new Event[] {new Event("Flood strikes...")};
+    private Event[] events = new Event[] {
+        new Event("Flood strikes..."), 
+        new Event("People have overthrown you..."), 
+        new Event("You have bankrupted yourself...")
+    };
 
     public void start() {
         for (Event event : events) {
             System.out.println(event);
+            sleep();
+        }
+    }
+
+    public static void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Thread was interrupted...");
         }
     }
 }
---
+++ src/UI.java
@@ -0,0 +1,10 @@
+class UI {
+    public static final String WELCOME_USER = "Hi there! Congratulations on being elected as a policymaker in Jakarta!\nYou have been tasked to manage the issue of flooding... but hey! It can't be that hard... right?\nJust widen the rivers and everything will be hunky dory!\n";
+
+    public void display(String message) {
+        for (String row : message.split("\n")) {
+            System.out.println(row);
+            Logic.sleep();
+        }
+    }
+}
