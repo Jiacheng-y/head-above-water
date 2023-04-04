bdf7146e6b9fb352c9d1b1c71c807f116aedea9c
Jiacheng-y <e0773814@u.nus.edu>
2023-04-04 15:47:06 +0800

    save game data to storage in main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ class Main {
     public static void main(String[] args) {
         UI.display(UI.WELCOME_USER);
         Logic logic = new Logic();
+        Storage storage = new Storage();
         logic.start();
+        storage.closeWriter();
     }
 }
