3bea9d88ae7e024fcadb0a0a2d4c4b27e6febd7e
nseah21 <nseah21@gmail.com>
2023-04-04 19:55:52 +0800

    Hook up event array from Utility to Logic

--- src/Utility.java
+++ src/Utility.java
@@ -6,15 +6,15 @@ import java.util.function.Supplier;
 
 class Utility {
     public static String RIVER_CANALISATION = String.format("River canalisation\nCost: -%d\nSatisfaction: +%d\nFlood resistance: +%d\n", 100, 7, 4);
-    public static String RIVER_NATURALISATION = String.format("River canalisation\nCost: -%d\nSatisfaction: +%d\nFlood resistance( only increases 3 rounds later): +%d\n", 2000, 2, 10);
-    public static String DO_NOTHING = String.format("River canalisation\nCost: -%d\nSatisfaction: +%d\nFlood resistance: +%d\n", 0, 0, 0);
+    public static String RIVER_NATURALISATION = String.format("River naturalisation\nCost: -%d\nSatisfaction: +%d\nFlood resistance( only increases 3 rounds later): +%d\n", 2000, 2, 10);
+    public static String DO_NOTHING = String.format("Do nothing \nCost: -%d\nSatisfaction: +%d\nFlood resistance: +%d\n", 0, 0, 0);
 
     // Event 1: land subsidence due to excessive groundwater extraction
     public final Event EVENT_ONE = new Event("Excessive groundwater extraction over the past few years have " +
             "led to significant land subsidence. As a result, the recent rainfall caused flooding in the city!", RIVER_CANALISATION,
             RIVER_NATURALISATION, DO_NOTHING);
     public final Event EVENT_TWO = new Event("", "", "", "");
-    public Event EVENT_THREE = new Event("Your colleagues are shunning you for your policies on flood prevention... \"Just normalise the river\", they say! If this goes on, the overall satisfaction of your candidacy will be impacted!", "Sure, river normalisation has been tried and tested... let's just go with the flow... it will cost $2000 dollars but their satisfaction will go up by 10", "The money is better spent on naturalisation though... $3000 dollars for an increase in flood resilience of 3 units per time step over 3 time steps", "I guess I'll just lay low for now...");
+    public final Event EVENT_THREE = new Event("Your colleagues are shunning you for your policies on flood prevention... \"Just normalise the river\", they say! If this goes on, the overall satisfaction of your candidacy will be impacted!", "Sure, river normalisation has been tried and tested... let's just go with the flow... it will cost $2000 dollars but their satisfaction will go up by 10", "The money is better spent on naturalisation though... $3000 dollars for an increase in flood resilience of 3 units per time step over 3 time steps", "I guess I'll just lay low for now...");
 
     public void initialise() {
         // set effect of the event
@@ -64,6 +64,6 @@ class Utility {
             return 0;
         });
     }
-    
-    public Event[] events = new Event[] {EVENT_ONE, EVENT_TWO, EVENT_THREE};
+
+    public Event[] events = new Event[] {EVENT_ONE, EVENT_THREE, EVENT_TWO};
 }
