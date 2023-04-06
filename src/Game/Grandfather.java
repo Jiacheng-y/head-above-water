42177a2237b943f26c1c6f4517db74a22b3d4a61
Jiacheng-y <e0773814@u.nus.edu>
2023-04-07 02:28:04 +0800

    Comment out update method for pq

--- src/Game/Grandfather.java
+++ src/Game/Grandfather.java
@@ -4,10 +4,11 @@ import java.util.PriorityQueue;
 
 class GrandFather {
     public static PriorityQueue<Effect> queue = new PriorityQueue<>();
-    
-    public static void update() {
-        queue.forEach(e -> e.decreaseCountdown());
-    }
+
+    //There is no need to call the update method
+//    public static void update() {
+//        queue.forEach(e -> e.decreaseCountdown());
+//    }
 
     public static void execute(int i) {
         while (queue.peek() != null && queue.peek().getCountdown() == i) {
